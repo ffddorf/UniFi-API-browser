87017f62ab18b025cb6a21d557b6497ff475fef2
Marcus Weiner <marcus.weiner@gmail.com>
2016-09-05 22:32:32 +0200

    Use environment variables for accessing controller

--- config.template.php
+++ config.template.php
@@ -23,10 +23,15 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-$controlleruser     = '';          // the user name for access to the Unifi Controller
-$controllerpassword = '';          // the password for access to the Unifi Controller
-$controllerurl      = '';          // full url to the Unifi Controller, eg. 'https://22.22.11.11:8443'
-$controllerversion  = '';          // the version of the Controller software, eg. '4.6.6' (must be at least 4.0.0)
+// the user name for access to the Unifi Controller
+$controlleruser     = $_ENV['UNIFI_USER'];
+// the password for access to the Unifi Controller
+$controllerpassword = $_ENV['UNIFI_PASSWORD'];
+// full url to the Unifi Controller, eg. 'https://22.22.11.11:8443'
+$controllerurl      = $_ENV['UNIFI_URL'];
+// the version of the Controller software, eg. '4.6.6' (must be at least 4.0.0)
+$controllerversion  = $_ENV['UNIFI_VERSION'];
+
 $cookietimeout      = '3600';      // time of inactivity in seconds, after which the PHP session cookie will be refreshed
                                    // after the cookie refresh the site and data collection will need to be selected again
 $theme              = 'bootstrap'; /* your default theme of choice, pick one from the list below
